7d87dfae0d1ba723309490a10a4c4de1f44e5a8a
espehel <espenhellerud5@gmail.com>
2014-03-21 13:12:57 +0100

    cleaned up code

--- Probe.java
+++ Probe.java
@@ -4,104 +4,22 @@ import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
-public class Probe implements Runnable, Serializable {
+public class Probe implements Runnable {
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 288210901973665886L;
-	private ArrayList<Integer> edges = new ArrayList<Integer>();
-	int lockOwner;
-	Server currentServer;
-	Transaction prevTransaction;
+
 	final ServerImpl server;
 	
 	public Probe(ServerImpl server){
 		this.server = server;
 	}
 	
-	
-	public Probe(int probeSender, int lockOwner, Server origin){
-		edges.add(probeSender);
-		this.lockOwner = lockOwner;
-		currentServer = origin;
-		server = null;
-	}
-
-	public Probe(int transaction1, int transaction2) {
-		edges.add(transaction1);
-		edges.add(transaction2);
-//		this.prevTransaction = prevTransaction;
-		server = null;
-	}
-	
-	
 	@Override
 	public void run(){
 		try {
-			server.receiveProbe(edges);
+			server.receiveProbe(new ArrayList<Integer>());
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-//	@Override
-//	public void run() {
-//		while (true) {
-//			//if the lockOwner creates a cycle it breaks, else it adds the lock owner to the list of transactions
-////			if(createsCycle(lockOwner))
-////				break;
-////			else
-////				edges.add(lockOwner);
-//			
-//			
-//			//Serveren som eier transaksjonenen som bruker ressursen som det ventes på
-//			currentServer = ((ServerImpl) currentServer).getServer(ServerImpl.getTransactionOwner(lockOwner));
-//			
-//			//transaksjonen som eier ressursjen for transaksjon ventet på
-//			Transaction currentTransaction;
-//			try {
-//				currentTransaction = ((ServerImpl)currentServer).getActiveTransaction();
-//			
-//			//hvis neste transaksjon venter på en ressurs legges den til edges lista, hvis ikke avsluttes probinga
-//			if(currentTransaction.isWaitingForResource())
-//				if(createsCycle(currentTransaction.getTransactionId())){
-//					abortTransaction(currentServer);
-//					break;//cycle must abort
-//				}
-//				else
-//					edges.add(currentTransaction.getTransactionId());
-//			else
-//				break;//everything will be ok
-//			
-//			ResourceAccess waitingForResource = currentTransaction.getWaitingForResource();
-//			System.out.println(waitingForResource);
-//			//we are now at the next transaction and sets the owner of the lock it is waiting for accordingly
-//			lockOwner = ((ServerImpl) waitingForResource.server).getResource(waitingForResource.resourceId).getLockOwner();
-//			} catch (RemoteException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//			
-//		}
-//		
-//		
-//	}
-	
-	private void abortTransaction(Server server) {
-		((ServerImpl)server).abortActiveTransaction();
-	}
-
-	public boolean createsCycle(int transId){
-		System.out.println(edges.size());
-		for (Integer id : edges) {
-			if(id == transId)
-				return true;
-		}
-		return false;
-	}
-	public void addEdge(int transId){
-		edges.add(transId);
 	}
 }
--- Resource.java
+++ Resource.java
@@ -5,111 +5,113 @@ package oving4;
  */
 class Resource
 {
-  static final int NOT_LOCKED = -1;
-  int abortTransactionId;
+	static final int NOT_LOCKED = -1;
+	int abortTransactionId;
 
-  /**
-   * The transaction currently holding the lock to this resource
-   */
-  private int lockOwner;
-  private Server owner;
+	/**
+	 * The transaction currently holding the lock to this resource
+	 */
+	private int lockOwner;
+	private Server owner;
 
-  /**
-   * Creates a new resource.
-   */
-  Resource(Server owner)
-  {
-    lockOwner = NOT_LOCKED;
-    this.owner = owner;
-  }
+	/**
+	 * Creates a new resource.
+	 */
+	Resource(Server owner)
+	{
+		lockOwner = NOT_LOCKED;
+		this.owner = owner;
+	}
 
-  /**
-   * Gives the lock of this resource to the requesting transaction. Blocks
-   * the caller until the lock could be acquired.
-   *
-   * @param transactionId The ID of the transaction that wants the lock.
-   * @return Whether or not the lock could be acquired.
-   */
-  synchronized boolean lock(int transactionId)
-  {
-    if (lockOwner == transactionId) {
-      System.err.println("Error: Transaction " + transactionId + " tried to lock a resource it already has locked!");
-      return false;
-    }
-    long start = System.currentTimeMillis();
-    while (lockOwner != NOT_LOCKED) {
-      try {
-    	if(Globals.USE_TIMEOUT){
-	        wait(3000);
-	        if(start<System.currentTimeMillis()-3000){
-	        	System.out.println("TIMEOUT");
-	        	return false;
-	        }
-    	}
-    	if(Globals.PROBING_ENABLED){
-    		wait();
-    		
-	        if(abortTransactionId == transactionId)
-	        	return false;
-    	}
-      } catch (InterruptedException ie) {
-      }
-    }
+	/**
+	 * Gives the lock of this resource to the requesting transaction. Blocks
+	 * the caller until the lock could be acquired.
+	 *
+	 * @param transactionId The ID of the transaction that wants the lock.
+	 * @return Whether or not the lock could be acquired.
+	 */
+	synchronized boolean lock(int transactionId)
+	{
+		if (lockOwner == transactionId) {
+			System.err.println("Error: Transaction " + transactionId + " tried to lock a resource it already has locked!");
+			return false;
+		}
+		//loggs the time before thread going to sleep for timeout purposes
+		long start = System.currentTimeMillis();
+		while (lockOwner != NOT_LOCKED) {
+			try {
+				//the thread will wake after 3000 if not woken before. When it is woken it will check if the time has gone past the timeout, if so it aborts
+				if(Globals.USE_TIMEOUT){
+					wait(3000);
+					if(start<System.currentTimeMillis()-Globals.TIMEOUT_INTERVAL){
+						return false;
+					}
+				}
+				//if a deadlock is detected all threads waiting on this object will be woken. All will go back waiting except one who will be aborted
+				if(Globals.PROBING_ENABLED){
+					wait();
+					//finds the transaction that needs to be aborted
+					if(abortTransactionId == transactionId)
+						return false;
+				}
+			} catch (InterruptedException ie) {
+			}
+		}
 
-    lockOwner = transactionId;
-    return true;
-  }
+		lockOwner = transactionId;
+		return true;
+	}
 
-  /**
-   * Releases the lock of this resource.
-   *
-   * @param transactionId The ID of the transaction that wants to release lock.
-   *                      If this transaction doesn't currently own the lock an
-   *                      error message is displayed.
-   * @return Whether or not the lock could be released.
-   */
-  synchronized boolean unlock(int transactionId)
-  {
-    if (lockOwner == NOT_LOCKED || lockOwner != transactionId) {
-      System.err.println("Error: Transaction " + transactionId + " tried to unlock a resource without owning the lock!");
-      return false;
-    }
+	/**
+	 * Releases the lock of this resource.
+	 *
+	 * @param transactionId The ID of the transaction that wants to release lock.
+	 *                      If this transaction doesn't currently own the lock an
+	 *                      error message is displayed.
+	 * @return Whether or not the lock could be released.
+	 */
+	synchronized boolean unlock(int transactionId)
+	{
+		if (lockOwner == NOT_LOCKED || lockOwner != transactionId) {
+			System.err.println("Error: Transaction " + transactionId + " tried to unlock a resource without owning the lock!");
+			return false;
+		}
 
-    lockOwner = NOT_LOCKED;
-    // Notify a waiting thread that it can acquire the lock
-    notifyAll();
-    return true;
-  }
+		lockOwner = NOT_LOCKED;
+		// Notify a waiting thread that it can acquire the lock
+		notifyAll();
+		return true;
+	}
 
-  /**
-   * Gets the current owner of this resource's lock.
-   *
-   * @return An Integer containing the ID of the transaction currently
-   * holding the lock, or NOT_LOCKED if the resource is unlocked.
-   */
-  synchronized int getLockOwner()
-  {
-    return lockOwner;
-  }
+	/**
+	 * Gets the current owner of this resource's lock.
+	 *
+	 * @return An Integer containing the ID of the transaction currently
+	 * holding the lock, or NOT_LOCKED if the resource is unlocked.
+	 */
+	synchronized int getLockOwner()
+	{
+		return lockOwner;
+	}
 
-  /**
-   * Unconditionally releases the lock of this resource.
-   */
-  synchronized void forceUnlock()
-  {
-    lockOwner = NOT_LOCKED;
-    // Notify a waiting thread that it can acquire the lock
-    notifyAll();
-  }
+	/**
+	 * Unconditionally releases the lock of this resource.
+	 */
+	synchronized void forceUnlock()
+	{
+		lockOwner = NOT_LOCKED;
+		// Notify a waiting thread that it can acquire the lock
+		notifyAll();
+	}
 
-  /**
-   * Checks if this resource's lock is held by a transaction running on the specified server.
-   *
-   * @param serverId The ID of the server.
-   * @return Whether or not the current lock owner is running on that server.
-   */
-  synchronized boolean isLockedByServer(int serverId)
-  {
-    return lockOwner != NOT_LOCKED && ServerImpl.getTransactionOwner(lockOwner) == serverId;
-  }
+	/**
+	 * Checks if this resource's lock is held by a transaction running on the specified server.
+	 *
+	 * @param serverId The ID of the server.
+	 * @return Whether or not the current lock owner is running on that server.
+	 */
+	synchronized boolean isLockedByServer(int serverId)
+	{
+		return lockOwner != NOT_LOCKED && ServerImpl.getTransactionOwner(lockOwner) == serverId;
+	}
 }
